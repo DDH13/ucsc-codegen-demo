fc0af048c57bb73e8d627043deb9aea06806e37b
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2023-09-17 12:25:04 +0530

    Add Orignal add integers code that was removed

---
+++ src/main/java/org/example/Add.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.List;
+
+public class Add {
+    Stmt[] stmt = new Stmt[]{
+            new Return(new IntOp(new IntOp(new Var("a"), new Var("b"), "+"), new Var("c"), "+"))
+    };
+    Func func = new Func("add", new Var[]{new Var("a"), new Var("b"), new Var("c")}, stmt);
+}
